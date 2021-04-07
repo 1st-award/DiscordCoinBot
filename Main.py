4bad67b60ec8edb4bb9a4f03c5f6dac0d7f4c96c
gomteang2dayo <as4290156@gmail.com>
2021-04-07 22:29:03 +0900

    BOT_TOKEN 부분 수정

--- Main.py
+++ Main.py
@@ -30,5 +30,5 @@ async def Ping(ctx):
     await ctx.send('Pong', delete_after=3.0)
 
 # 절대로 토큰을 넣어둔 체 푸시 하지 말 것!
-access_token = os.environ["BOT_TOKEN"]
+access_token = os.environ['BOT_TOKEN']
 bot.run(access_token)
